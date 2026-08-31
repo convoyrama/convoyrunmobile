1ff3686492ff30dfa969cd0879cff4f2a9610309
convoyrama <167719926+convoyrama@users.noreply.github.com>
2026-08-31 15:37:10 -0300

    0.1.0fix1

--- app/src/main/java/com/convoyrama/convoyrun/MainActivity.kt
+++ app/src/main/java/com/convoyrama/convoyrun/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.convoyrama.convoyrun.data.PreferencesManager
 import com.convoyrama.convoyrun.model.ConvoyEvent
 import com.convoyrama.convoyrun.p2p.P2pManager
